7508e7f48a7d64cab92ac08cdf39d5b5c903110a
Vebe <fikri1838@gmail.com>
2022-06-23 22:59:15 +0700

    Update

---
+++ app/src/main/java/com/example/fishkuy/Ikanpayau.java
@@ -0,0 +1,39 @@
+package com.example.fishkuy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Ikanpayau extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuikan_payau);
+    }
+
+    public void back5(View view) {
+        Intent intent = new Intent(Ikantawar.this, Homescreen.class);
+        startActivity(intent);
+    }
+
+    public void beli5(View view) {
+        Intent intent2 = new Intent(Ikantawar.this, Detailproduk.class);
+        startActivity(intent2);
+    }
+
+    public void chat(View view) {
+        Intent intent5 = new Intent(Ikantawar.this, Chat.class);
+        startActivity(intent5);
+    }
+
+    public void keranjang(View view) {
+        Intent intent9 = new Intent(Ikantawar.this, Keranjang.class);
+        startActivity(intent9);
+    }
+
+    public void lacak(View view) {
+        Intent intent = new Intent(Ikantawar.this, Lacak.class);
+        startActivity(intent);
+    }
+}
